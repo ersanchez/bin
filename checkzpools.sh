bdc5794574d079a3ab0e63f346dfe5f5059b1a55
Eduardo Sanchez <eduardo@sanchez.nu>
2015-08-30 22:50:24 -0400

    initial commmit

---
+++ checkzpools.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Purpose: send an email notification if zpool is not healthy
+
+# loosely based on 
+# http://lawrit.lawr.ucdavis.edu/itdocs/it/how-to/automated-alerts-and-scrubbing-for-zfs-pools
+
+# Variables
+
+poolstatus=`zpool status -x`
+poolhost=`hostname -s`
+notifyemail=`cat /home/ers/doc/notification-email`
+notifysms=`cat /home/ers/doc/notification-sms`
+notifymsg=`cat /home/ers/doc/notification-sms-msg`
+
+if [ "$poolstatus" != "all pools are healthy" ] ; then
+	zpool status | mailx -s "ZPOOL ERROR on $poolhost" $notifyemail
+fi
+
+if [ "$poolstatus" = "all pools are healthy" ] ; then
+	echo " " | mailx -s "ZPOOL ERROR on $poolhost" $notifysms
+fi
