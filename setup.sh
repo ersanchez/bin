50c29715eef3af3381b720e5b80be5a948f1bda8
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-01-10 10:22:54 -0500

    initial commit
    This file will eventually be used to setup new Ubuntu machines and VMs.

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This shell script sets up Eduardo's working environment on a new 
+# Ubuntu machine.
+
+# Install applications
+sudo apt-get install -y curl
+sudo apt-get install -y git
+sudo apt-get install -y pandoc
+sudo apt-get install -y tmux
+sudo apt-get install -y vim
+sudo apt-get install -y xclip
+
+# git pull dotfiles
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+# git install dotfiles
+git clone https://github.com/ersanchez/bin/dotfiles
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sb dotfiles/.taskrc .
+ln -sb dotfiles/.tmux.conf .
+ln -sb dotfiles/.vimrc .
