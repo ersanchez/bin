7476adf79fb8110992fc4116a7637511118f81c3
Eduardo Sanchez <eduardo@sanchez.nu>
2016-01-18 15:32:33 -0500

    Changed date to 8601; changed log location

--- task_sync.sh
+++ task_sync.sh
@@ -2,4 +2,5 @@
 
 /usr/local/bin/task sync
 
-echo "taskwarrior sync completed: $(date --iso-8601=seconds -u)" >> $HOME/.task/task-sync.log
+#echo "taskwarrior sync completed: $(date --iso-8601=seconds -u)" >> $HOME/.task/task-sync.log
+echo -e "$(date --iso-8601=seconds -u)\n" >> /home/ers/log/cron_tasksync
