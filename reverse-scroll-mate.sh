1583797a04ec464f33b74018535e0fa9f49b388b
Eduardo Sanchez <eduardo@sanchez.nu>
2016-01-22 09:31:37 -0500

    Added comments

--- reverse-scroll-mate.sh
+++ reverse-scroll-mate.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# This script enables reverse scrolling in Ubuntu Mate.
+# I couldn't figure out how to do this permanently, so this kludge will
+# have to suffice.
 synclient VertScrollDelta=-111
