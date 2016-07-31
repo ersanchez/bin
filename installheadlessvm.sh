f0ea7d6d17843d0fe25b74e8048488bd8bb2b76c
Eduardo Sanchez <eduardo@sanchez.nu>
2016-07-31 15:54:41 -0400

    added line to turn on virtual display

--- installheadlessvm.sh
+++ installheadlessvm.sh
@@ -12,6 +12,8 @@ echo "* name for the new VM"
 echo "* ostype - enter \"VBoxManage list ostypes\" if you are not sure"
 echo "* URL for the ISO file you will install"
 echo "* URL for the VirtualBox Extension Pack to install"
+echo ""
+echo "NOTE: VBox Extension Pack for this version must already be installed."
 
 # Installation information
 echo "Enter VM name: "
@@ -28,6 +30,7 @@ echo "Type in the full path to the ISO file to install: "
 read vmpathtoiso
 echo $vmpathtoiso
 
+
 # Creating the VM
 VBoxManage createvm --name $vmname --register
 VBoxManage modifyvm $vmname --memory 2048 --acpi on --boot1 dvd --nic1 bridged --bridgeadapter1 $active_eth
@@ -35,3 +38,4 @@ VBoxManage createhd --filename $HOME/VirtualBox\ VMs/$vmname/"$vmname".vdi --siz
 VBoxManage storagectl $vmname --name "IDE Controller" --add ide
 VBoxManage storageattach $vmname --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium $HOME/VirtualBox\ VMs/$vmname/"$vmname".vdi
 VBoxManage storageattach $vmname --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $vmpathtoiso
+VBoxManage modifyvm $vmname --vrde on
