b5462204cf88984ffff07887984c4925a9867ce4
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-09-03 16:32:58 -0400

    moved from apt-get to the new "

--- setup.sh
+++ setup.sh
@@ -4,39 +4,37 @@
 # Ubuntu machine.
 #
 # Install applications
-sudo apt-get install -y abcde
-sudo apt-get install -y curl
-sudo apt-get install -y git
-sudo apt-get install -y pandoc
-sudo apt-get install -y synaptic
-sudo apt-get install -y tmux
-sudo apt-get install -y vim
-sudo apt-get install -y xclip
-sudo apt-get install -y youtube-dl
-sudo apt-get install -y fonts-inconsoloata
-sudo apt-get install -y cmake
-sudo apt-get install -y gnutls-bin
-sudo apt-get install -y pv
-sudo apt-get install -y xclip
-sudo apt-get install -y taskwarrior
-sudo apt-get install -y gnupg2
-sudo apt-get install -y gparted
-sudo apt-get install -y lyx
-sudo apt-get install -y markdown
-sudo apt-get install -y nfs-common
-sudo apt-get install -y okular
-sudo apt-get install -y par2
-sudo apt-get install -y unrar
-sudo apt-get install -y rsync
-sudo apt-get install -y shutter
-sudo apt-get install -y syncthing
-sudo apt-get install -y tor-browser
-sudo apt-get install -y ubuntu-mate-welcom
-sudo apt-get install -y vlc
-sudo apt-get install -y goldendict
-sudo apt-get install -y tree
-sudo apt-get install -y syncthing
-sudo apt-get install -y ubuntu-mate-welcome
+sudo apt install -y abcde
+sudo apt install -y cmake
+sudo apt install -y curl
+sudo apt install -y fonts-inconsoloata
+sudo apt install -y git
+sudo apt install -y gnupg2
+sudo apt install -y gnutls-bin
+sudo apt install -y goldendict
+sudo apt install -y gparted
+sudo apt install -y keepass2
+sudo apt install -y lyx
+sudo apt install -y markdown
+sudo apt install -y mono-complete
+sudo apt install -y nfs-common
+sudo apt install -y okular
+sudo apt install -y pandoc
+sudo apt install -y par2
+sudo apt install -y pv
+sudo apt install -y rsync
+sudo apt install -y shutter
+sudo apt install -y synaptic
+sudo apt install -y syncthing
+sudo apt install -y taskwarrior
+sudo apt install -y tmux
+sudo apt install -y tree
+sudo apt install -y ubuntu-mate-welcome
+sudo apt install -y unrar
+sudo apt install -y vim
+sudo apt install -y vlc
+sudo apt install -y xclip
+sudo apt install -y youtube-dl
 
 # git pull dotfiles
 cd $HOME
@@ -45,13 +43,13 @@ if [ -d ./dotfiles/ ]; then
 fi
 # git install dotfiles
 git clone https://github.com/ersanchez/dotfiles.git
-ln -sb dotfiles/.abcde .
-ln -sb dotfiles/.bash_logout .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.gitconfig .
-ln -sb dotfiles/.gitignore_global .
-ln -sb dotfiles/.profile .
-ln -sb dotfiles/.tmux.conf .
-ln -sb dotfiles/.vimrc .
+ln -sb git-files/dotfiles/.abcde .
+ln -sb git-files/dotfiles/.bash_logout .
+ln -sb git-files/dotfiles/.bash_profile .
+ln -sb git-files/dotfiles/.bashrc .
+ln -sb git-files/dotfiles/.bashrc_custom .
+ln -sb git-files/dotfiles/.gitconfig .
+ln -sb git-files/dotfiles/.gitignore_global .
+ln -sb git-files/dotfiles/.profile .
+ln -sb git-files/dotfiles/.tmux.conf .
+ln -sb git-files/dotfiles/.vimrc .
