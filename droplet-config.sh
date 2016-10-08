45eb91806946a5bc177b6e8b103da5a6fc2cd313
Eduardo Sanchez <e@eduardosanchez.me>
2016-10-08 10:48:17 -0400

    added section on SSH (TESTING ONLY)

--- droplet-config.sh
+++ droplet-config.sh
@@ -12,23 +12,30 @@
 # Create new user
 echo "Enter the name of the user to be added: "
 read username
-echo $username
 
 echo "Would you like to enter a specific UID? (y/n) "
 read quid
-echo $quid
 if [ $quid = "y" ]; then
 	echo "Enter the UID for the new user: "
 	read newuseruid
-	echo $newuseruid
 	adduser --uid $newuseruid $username
 else
 	adduser $username
 fi
 
+# Add user to sudoers?
 echo "Add new user to sudoers? (y/n) "
 read sudodecision
 echo $sudodecision
 if [ $sudodecision="y" ]; then
 	adduser $username sudo
 fi
+
+# Configure SSH for new user
+#
+# DO NOT USE THIS ON A PRODUCTION MACHINE !!TESTING ONLY!!
+mkdir /home/$username/.ssh
+chmod 700 /home/$username/.ssh
+touch /home/$username/.ssh/authorized_keys
+cat /home/root/authorized_keys > /home/$username/authorized_keys
+chown -R $username:$username /home/$username/.ssh
