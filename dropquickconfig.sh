8569b119e6f630bdac7ffdcece696c9c0756cbc9
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-10-08 15:04:25 -0400

    added pointer to droplet-adduser.sh

--- dropquickconfig.sh
+++ dropquickconfig.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+#
+# This is a TESTING-ONLY script to quickly configure a DigitalOcean droplet.
+#
+# !!! DO NOT USE THIS IN PRODUCTION !!!
+#
+# install git
 bash <(curl -s https://raw.githubusercontent.com/ersanchez/bin/master/installgit.sh)
+# add user
+bash <(curl -s https://raw.githubusercontent.com/ersanchez/bin/master/droplet-adduser.sh)
