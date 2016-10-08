a4a4faafead65b8fe2db1515d9d5b93c8368234f
Eduardo Sanchez <e@eduardosanchez.me>
2016-10-08 15:18:26 -0400

    done

--- drop2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e  # exit if any command exits with a non-zero (fail) status
-apt-get install -y git
-mkdir /root/git-files
-cd /root/git-files
-git clone https://github.com/ersanchez/bin.git
