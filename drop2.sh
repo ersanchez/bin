a11357a841d8d7b0f2cfef8ce978a6e239d43dc5
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-10-08 13:38:14 -0400

    enclosed git install in an "if" statement to track success/fail

--- drop2.sh
+++ drop2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-apt-get install -y git
+if "apt-get install -y git" ; then
+    echo "Git installation successful"
+else
+    echo "Git installation failed"
+fi
+
 mkdir /root/git-files
 cd /root/git-files
 git clone https://github.com/ersanchez/bin.git
