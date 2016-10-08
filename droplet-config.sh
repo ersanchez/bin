a16a1e110539f4b4ca37de59ae7295a6fea2e99a
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-10-08 14:06:53 -0400

    added an "exit 0" to if/then; enclosed command in ` `

--- droplet-config.sh
+++ droplet-config.sh
@@ -8,10 +8,11 @@ set -e  # exit if any command exits with a non-zero (fail) status
 # !!! FOR TESTING PURPOSES ONLY !!!
 # Run as root
 #
-if [ "apt-get install -y git" ]; then
+if [ "`apt-get install -y git`" ]; then
     echo "Git install success"
 else
     echo "Git install failed"
+    exit 1
 fi
 
 mkdir /root/git-files
