85129c0cc43210dc96c1667ee59cb4cfe47e9750
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-10-30 18:38:22 -0400

    changed file name, removed dotfile scripts

---
+++ install-server-apps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This shell script sets up Eduardo's working environment on a new 
+# Ubuntu machine.
+#
+# Install applications
+sudo apt install -y cmake
+sudo apt install -y curl
+sudo apt install -y ecryptfs-utils
+sudo apt install -y git
+sudo apt install -y gnupg2
+sudo apt install -y gnutls-bin
+sudo apt install -y markdown
+sudo apt install -y pv
+sudo apt install -y rsync
+sudo apt install -y syncthing
+sudo apt install -y taskwarrior
+sudo apt install -y tmux
+sudo apt install -y tree
+sudo apt install -y unrar
+sudo apt install -y vim-tiny
+sudo apt install -y xclip
+sudo apt install -y youtube-dl
