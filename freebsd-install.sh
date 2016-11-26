f86bcd662f7ae06687faf6400488f0b139bf14b2
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2016-11-26 14:43:13 -0500

    added first list of applications to install

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -1,3 +1,15 @@
 #/bin/sh
 
 # Script to automatically configure a headless FreeBSD server
+
+# Install pkg
+sudo pkg install -y python3
+sudo pkg install -y rsync
+sudo pkg install -y ruby
+sudo pkg install -y ssmtp
+sudo pkg install -y testdisk
+sudo pkg install -y tmux
+sudo pkg install -y vim-lite
+sudo pkg install -y wget
+sudo pkg install -y xclip
+sudo pkg install -y zfstools
