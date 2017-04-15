82c75079dd6113e1715b44e373205eb83e82bdc6
Eduardo Sanchez <e@eduardosanchez.me>
2017-04-15 10:11:28 -0400

    added wifi-radar

--- installdesktop.sh
+++ installdesktop.sh
@@ -70,6 +70,7 @@ sudo apt install -y virtualbox
 sudo apt install -y vlc
 sudo apt install -y vokoscreen
 sudo apt install -y whois
+sudo apt install -y wifi-radar
 sudo apt install -y wine
 sudo apt install -y xclip
 #sudo apt install -y youtube-dl
