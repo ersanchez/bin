d48f4b437cee7afc7cb08f6e88d4aeea13635180
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2017-07-29 09:14:22 -0400

    start for-loop for creating symbolic links

--- redirecthomefolders.sh
+++ redirecthomefolders.sh
@@ -6,3 +6,23 @@
 
 # It is intended to be run on a GNU/Linux distribution - Ubuntu.
 
+directories=(
+  '$HOME/bin'
+  '$HOME/Documents'
+  '$HOME/Music'
+  '$HOME/Pictures'
+  '$HOME/Public'
+  '$HOME/Templates'
+  '$HOME/Videos'
+)
+
+# To Do: include section to backup existing directories
+#    This will save my bacon if accidentally run on 
+#    home directory with existing files.
+
+for filename in "${directories[@]}"; do
+  # Delete the existing directory
+  rm -rf "$directories"
+  # Replace it with a hard link to the Dropbox file
+  ln $HOME/ #need to complete this symbolic linking
+done
