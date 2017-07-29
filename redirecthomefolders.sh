c2c9da0a2a7ca37cb8e7cbb78a6bcfd186612900
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2017-07-29 09:02:41 -0400

    initial commit

---
+++ redirecthomefolders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The purpose of this script is to configure a fresh install such that
+# typical /home/user folders are linked to Syncthing folders that are
+# synchronized with other user computers.
+
+# It is intended to be run on a GNU/Linux distribution - Ubuntu.
+
