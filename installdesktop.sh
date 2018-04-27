9d8ff9adf971c936600fc974ed9aabe3bf47d7a6
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2018-04-27 17:54:45 -0400

    Update installdesktop.sh

--- installdesktop.sh
+++ installdesktop.sh
@@ -169,10 +169,10 @@ if [ -d $HOME/Videos ]; then
 
 mkdir .st-bin .st-docs .st-mus .st-pic .st-pub .st-vid
 
-ln -sb $HOME/.st-bin $HOME/bin/
-ln -sb $HOME/.st-docs $HOME/Documents/
-ln -sb $HOME/.st-mus $HOME/Music/
-ln -sb $HOME/.st-pic $HOME/Pictures/
-ln -sb $HOME/.st-pub $HOME/Public/
-ln -sb $HOME/.st-vid $HOME/Videos/
+ln -sb $HOME/.st-bin/ $HOME/bin
+ln -sb $HOME/.st-docs/ $HOME/Documents
+ln -sb $HOME/.st-mus/ $HOME/Music
+ln -sb $HOME/.st-pic/ $HOME/Pictures
+ln -sb $HOME/.st-pub/ $HOME/Public
+ln -sb $HOME/.st-vid/ $HOME/Videos
 
