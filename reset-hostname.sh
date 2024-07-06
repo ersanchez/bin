de4430e5a674b488b9a784ae72f5672a1347d90f
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2024-07-06 16:45:51 -0400

    restructured

--- reset-hostname.sh
+++ reset-hostname.sh
@@ -4,15 +4,7 @@
 #  1. prompting the user to enter a new hostname
 #  2. applying that new hostname
 
-#update_etc_hosts() {
-#    local new_hostname=$1
-#    local old_hostname=$(hostname)
-
-    # update /etc/hosts with the new hostname
-#    sed -i "s/$old_hostname/$new_hostname/g" /etc/hosts
-#}
-local new_hostname=$1
-local old_hostname=$(hostname)
+old_hostname=$(hostname)
 
 # Prompt the user for the new hostname
 read -p "Enter the new hostname: " new_hostname
