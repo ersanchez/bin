b80599cf98f644bec91ee8d3a1380f00093d09fb
Eduardo Sanchez <ersanchez@users.noreply.github.com>
2024-07-06 15:18:15 -0400

    initial commit

---
+++ reset-hostname.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script resets the hostname by:
+#  1. prompting the user to enter a new hostname
+#  2. applying that new hostname
+
+update_etc_hosts() {
+    local new_hostname=$1
+	local old_hostname=$(hostname)
+
+	# update /etc/hosts with the new hostname
+	sed -i "s/$old_hostname/$new_hostname/g" /etc/hosts
+}
+
+# Prompt the user for the new hostname
+read -p "Enter the new hostname: " new_hostname
+
+# Setup the new hostname using systemd
+hostnamectl set-hostname "$new_hostname"
+
+# Update the /etc/hosts file using the function at the top
+update_etc_hosts "$new_hostname"
+
+# Restart systemd-hostnamed service
+systemctl restart systemd-hostnamed
+
+# Echo the new hostname
+echo "The hostname has been updated to: " $(hostname)
